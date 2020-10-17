6dc487652d5cfacb879d253a846a93ca9fa9d426
Steven Côté <scote01@hotmail.com>
2020-10-16 20:06:44 -0400

    Project completed - sentiment web app functional

--- serve/predict.py
+++ serve/predict.py
@@ -69,14 +69,17 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    
+    input_w = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, input_w)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
-    data_pack = np.hstack((data_len, data_X))
-    data_pack = data_pack.reshape(1, -1)
+    #data_pack = np.hstack((data_len, data_X))
+    #data_pack = data_pack.reshape(1, -1)
+    
+    data_pack = np.append(data_len, data_X)
+    data_pack = data_pack[None, :]
     
     data = torch.from_numpy(data_pack)
     data = data.to(device)
@@ -87,6 +90,7 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
-
+    result = model(data)
+    result = result.detach().numpy()
+    
     return result
--- train/train.py
+++ train/train.py
@@ -69,7 +69,35 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     
     # TODO: Paste the train() method developed in the notebook here.
 
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+               
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            
+            # zero accumulated gradients
+            model.zero_grad()
+
+            # get the output from the model
+            output = model(batch_X)
+
+            # calculate the loss and perform backprop
+            loss = loss_fn(output.squeeze(), batch_y.float())
+            loss.backward()
+            # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+            # nn.utils.clip_grad_norm_(net.parameters(), clip)
+            optimizer.step()
+            
+            
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
+
 
 
 if __name__ == '__main__':
